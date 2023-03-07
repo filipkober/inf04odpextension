0776318cd9d280c19ec49f5e5fb57f32b6278c80
Filip Kober <koberfilip2@gmail.com>
2023-03-07 16:28:16 +0100

    removed typescript

--- src/inf04-40.js
+++ src/inf04-40.js
@@ -1,99 +1,40 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-fetch("https://inf04.nigdit.men/api/questions").then(function (res) { return res.json(); }).then(function (data) { return __awaiter(void 0, void 0, void 0, function () {
-    var clickButtons, highlightColor, delay;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, browser.storage.sync.get('clickButtons').then(function (res) { return res.clickButtons; })];
-            case 1:
-                clickButtons = _a.sent();
-                return [4 /*yield*/, browser.storage.sync.get('highlightColor').then(function (res) { return res.highlightColor; })];
-            case 2:
-                highlightColor = _a.sent();
-                return [4 /*yield*/, browser.storage.sync.get('delay').then(function (res) { return res.delay; })];
-            case 3:
-                delay = _a.sent();
-                setTimeout(function () { return zaznaczPytania(data, { clickButtons: clickButtons || false, highlightColor: highlightColor || "#008000" }); }, delay || 1000);
-                return [2 /*return*/];
+fetch("https://inf04.nigdit.men/api/questions").then((res) => res.json()).then( async (data) => {    
+    const clickButtons = await browser.storage.sync.get('clickButtons').then((res) => res.clickButtons);
+    const highlightColor = await browser.storage.sync.get('highlightColor').then((res) => res.highlightColor);
+    const delay = await browser.storage.sync.get('delay').then((res) => res.delay);
+    setTimeout(() => zaznaczPytania(data, {clickButtons: clickButtons || false, highlightColor: highlightColor || "#008000"}), delay || 1000)
+}).catch((err) => console.error(err));
+
+const zaznaczPytania = async (odpowiedzi, {clickButtons = false, highlightColor = "#008000"}) => {
+    const pytania = document.querySelectorAll('.trescE');
+    for (let i = 1; i <= pytania.length; i++) {
+        const elementPytanie = pytania[i - 1];
+        const odpowiedzA = document.querySelector(`#odpa${i}`);
+        const odpowiedzB = document.querySelector(`#odpb${i}`);
+        const odpowiedzC = document.querySelector(`#odpc${i}`);
+        const odpowiedzD = document.querySelector(`#odpd${i}`);        
+        if(!elementPytanie || !odpowiedzA || !odpowiedzB || !odpowiedzC || !odpowiedzD) throw new Error('[ROZSZERZENIE]: Brak pytania lub odpowiedzi');
+        const trescPytania = elementPytanie.textContent?.slice(3);
+        let odpowiedzNaPytanie = odpowiedzi.find(p => trescPytania?.toLowerCase().includes(p.tresc.toLowerCase()));
+        if(!odpowiedzNaPytanie) {
+            const pierwsze10Znakow = trescPytania?.substring(5, 15).toLowerCase();
+            console.warn("[ROZSZERZENIE]: Nie znaleziono odpowiedzi na pytanie, szukam w miarę dopasowanej odpowiedzi");
+            
+            odpowiedzNaPytanie = odpowiedzi.find(q => q.tresc.toLowerCase().includes(pierwsze10Znakow || ''));
         }
-    });
-}); }).catch(function (err) { return console.error(err); });
-var zaznaczPytania = function (odpowiedzi, _a) {
-    var _b = _a.clickButtons, clickButtons = _b === void 0 ? false : _b, _c = _a.highlightColor, highlightColor = _c === void 0 ? "#008000" : _c;
-    return __awaiter(void 0, void 0, void 0, function () {
-        var pytania, _loop_1, i;
-        var _d, _e;
-        return __generator(this, function (_f) {
-            pytania = document.querySelectorAll('.trescE');
-            _loop_1 = function (i) {
-                var elementPytanie = pytania[i - 1];
-                var odpowiedzA = document.querySelector("#odpa".concat(i));
-                var odpowiedzB = document.querySelector("#odpb".concat(i));
-                var odpowiedzC = document.querySelector("#odpc".concat(i));
-                var odpowiedzD = document.querySelector("#odpd".concat(i));
-                if (!elementPytanie || !odpowiedzA || !odpowiedzB || !odpowiedzC || !odpowiedzD)
-                    throw new Error('[ROZSZERZENIE]: Brak pytania lub odpowiedzi');
-                var trescPytania = (_d = elementPytanie.textContent) === null || _d === void 0 ? void 0 : _d.slice(3);
-                var odpowiedzNaPytanie = odpowiedzi.find(function (p) { return trescPytania === null || trescPytania === void 0 ? void 0 : trescPytania.toLowerCase().includes(p.tresc.toLowerCase()); });
-                if (!odpowiedzNaPytanie) {
-                    var pierwsze10Znakow_1 = trescPytania === null || trescPytania === void 0 ? void 0 : trescPytania.substring(5, 15).toLowerCase();
-                    console.warn("[ROZSZERZENIE]: Nie znaleziono odpowiedzi na pytanie, szukam w miarę dopasowanej odpowiedzi");
-                    odpowiedzNaPytanie = odpowiedzi.find(function (q) { return q.tresc.toLowerCase().includes(pierwsze10Znakow_1 || ''); });
+        
+        if(!odpowiedzNaPytanie) throw new Error('[ROZSZERZENIE]: Nie znaleziono odpowiedzi na pytanie');
+        
+        const odpowiedziNaPytanie = [odpowiedzA, odpowiedzB, odpowiedzC, odpowiedzD];
+
+        for(const odpowiedz of odpowiedziNaPytanie) {
+            if(odpowiedz.textContent?.slice(3).toLowerCase().trim() === odpowiedzNaPytanie.odpowiedz.toLowerCase().trim()) {
+                if(clickButtons) {
+                    odpowiedz.click();
+                }else {
+                    odpowiedz.style.backgroundColor = highlightColor;
                 }
-                if (!odpowiedzNaPytanie)
-                    throw new Error('[ROZSZERZENIE]: Nie znaleziono odpowiedzi na pytanie');
-                var odpowiedziNaPytanie = [odpowiedzA, odpowiedzB, odpowiedzC, odpowiedzD];
-                for (var _i = 0, odpowiedziNaPytanie_1 = odpowiedziNaPytanie; _i < odpowiedziNaPytanie_1.length; _i++) {
-                    var odpowiedz = odpowiedziNaPytanie_1[_i];
-                    if (((_e = odpowiedz.textContent) === null || _e === void 0 ? void 0 : _e.slice(3).toLowerCase().trim()) === odpowiedzNaPytanie.odpowiedz.toLowerCase().trim()) {
-                        if (clickButtons) {
-                            odpowiedz.click();
-                        }
-                        else {
-                            odpowiedz.style.backgroundColor = highlightColor;
-                        }
-                    }
-                }
-            };
-            for (i = 1; i <= pytania.length; i++) {
-                _loop_1(i);
             }
-            return [2 /*return*/];
-        });
-    });
-};
+        }
+    }
+}
--- src/inf04.js
+++ src/inf04.js
@@ -1,104 +1,44 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-fetch("https://inf04.nigdit.men/api/questions").then(function (res) { return res.json(); }).then(function (data) { return __awaiter(void 0, void 0, void 0, function () {
-    var clickButtons, highlightColor, delay, newButton;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, browser.storage.sync.get('clickButtons').then(function (res) { return res.clickButtons; })];
-            case 1:
-                clickButtons = _a.sent();
-                return [4 /*yield*/, browser.storage.sync.get('highlightColor').then(function (res) { return res.highlightColor; })];
-            case 2:
-                highlightColor = _a.sent();
-                return [4 /*yield*/, browser.storage.sync.get('delay').then(function (res) { return res.delay; })];
-            case 3:
-                delay = _a.sent();
-                newButton = document.querySelector('#losujnowe');
-                if (!newButton)
-                    throw new Error('Brak przycisku');
-                setTimeout(function () { return nowePytanie(data, { clickButtons: clickButtons || false, highlightColor: highlightColor || "#008000", delay: delay || 1000 }); }, delay || 1000);
-                return [2 /*return*/];
+fetch("https://inf04.nigdit.men/api/questions").then((res) => res.json()).then( async (data) => {
+    const clickButtons = await browser.storage.sync.get('clickButtons').then((res) => res.clickButtons);
+    const highlightColor = await browser.storage.sync.get('highlightColor').then((res) => res.highlightColor);
+    const delay = await browser.storage.sync.get('delay').then((res) => res.delay);
+    const newButton = document.querySelector('#losujnowe');
+    if(!newButton) throw new Error('Brak przycisku');
+    setTimeout(() => nowePytanie(data, {clickButtons: clickButtons || false, highlightColor: highlightColor || "#008000", delay: delay || 1000}), delay || 1000)
+}).catch((err) => console.error(err));
+
+const nowePytanie = async (odpowiedzi, {clickButtons = false, highlightColor = "#008000", delay = 1000}) => {
+    if(!(!!odpowiedzi)) throw new Error('[ROZSZERZENIE]: Brak odpowiedzi')
+    const obecnePytania = document.querySelector('.tresc')
+    const odpowiedzA = document.querySelector('#odpa')
+    const odpowiedzB = document.querySelector('#odpb')
+    const odpowiedzC = document.querySelector('#odpc')
+    const odpowiedzD = document.querySelector('#odpd')
+    if(!obecnePytania || !odpowiedzA || !odpowiedzB || !odpowiedzC || !odpowiedzD) throw new Error('[ROZSZERZENIE]: Brak pytania lub odpowiedzi');
+    const obecnePytanie = obecnePytania.textContent;
+    if(!obecnePytanie) throw new Error('[ROZSZERZENIE]: Brak pytania');
+    const wszystkieOdp = [odpowiedzA, odpowiedzB, odpowiedzC, odpowiedzD];
+    let odpowiedz = odpowiedzi.find((pytanie) => pytanie.tresc.toLowerCase().includes(obecnePytanie.toLowerCase()));
+    if(!odpowiedz && !document.querySelector('.odpgood')) {
+        console.warn('[ROZSZERZENIE]: Nie znaleziono odpowiedzi na pytanie: ' + obecnePytanie);
+        const looseSearch = odpowiedzi.find((pytanie) => pytanie.tresc.substring(0, 10) === obecnePytanie.substring(0, 10));
+        console.log('[ROZSZERZENIE]: Szukanie w miarę dopasowanej odpowiedzi: ' + looseSearch?.tresc);
+        
+        if(!looseSearch?.odpowiedz) {
+            return setTimeout(nowePytanie, delay, odpowiedzi, {clickButtons, highlightColor, delay});
         }
-    });
-}); }).catch(function (err) { return console.error(err); });
-var nowePytanie = function (odpowiedzi, _a) {
-    var _b = _a.clickButtons, clickButtons = _b === void 0 ? false : _b, _c = _a.highlightColor, highlightColor = _c === void 0 ? "#008000" : _c, _d = _a.delay, delay = _d === void 0 ? 1000 : _d;
-    return __awaiter(void 0, void 0, void 0, function () {
-        var obecnePytania, odpowiedzA, odpowiedzB, odpowiedzC, odpowiedzD, obecnePytanie, wszystkieOdp, odpowiedz, looseSearch, _i, wszystkieOdp_1, ans, htmlAns;
-        return __generator(this, function (_e) {
-            if (!(!!odpowiedzi))
-                throw new Error('[ROZSZERZENIE]: Brak odpowiedzi');
-            obecnePytania = document.querySelector('.tresc');
-            odpowiedzA = document.querySelector('#odpa');
-            odpowiedzB = document.querySelector('#odpb');
-            odpowiedzC = document.querySelector('#odpc');
-            odpowiedzD = document.querySelector('#odpd');
-            if (!obecnePytania || !odpowiedzA || !odpowiedzB || !odpowiedzC || !odpowiedzD)
-                throw new Error('[ROZSZERZENIE]: Brak pytania lub odpowiedzi');
-            obecnePytanie = obecnePytania.textContent;
-            if (!obecnePytanie)
-                throw new Error('[ROZSZERZENIE]: Brak pytania');
-            wszystkieOdp = [odpowiedzA, odpowiedzB, odpowiedzC, odpowiedzD];
-            odpowiedz = odpowiedzi.find(function (pytanie) { return pytanie.tresc.toLowerCase().includes(obecnePytanie.toLowerCase()); });
-            if (!odpowiedz && !document.querySelector('.odpgood')) {
-                console.warn('[ROZSZERZENIE]: Nie znaleziono odpowiedzi na pytanie: ' + obecnePytanie);
-                looseSearch = odpowiedzi.find(function (pytanie) { return pytanie.tresc.substring(0, 10) === obecnePytanie.substring(0, 10); });
-                console.log('[ROZSZERZENIE]: Szukanie w miarę dopasowanej odpowiedzi: ' + (looseSearch === null || looseSearch === void 0 ? void 0 : looseSearch.tresc));
-                if (!(looseSearch === null || looseSearch === void 0 ? void 0 : looseSearch.odpowiedz)) {
-                    return [2 /*return*/, setTimeout(nowePytanie, delay, odpowiedzi, { clickButtons: clickButtons, highlightColor: highlightColor, delay: delay })];
-                }
-                odpowiedz = looseSearch;
+        odpowiedz = looseSearch;
+    }
+    for(const ans of wszystkieOdp) {
+        if(ans.textContent && ans.textContent.toLowerCase().slice(3) == odpowiedz?.odpowiedz.toLowerCase() && ans.className != 'odpgood') {
+            const htmlAns = ans;
+            if(clickButtons) {
+                htmlAns.click();
+            }else {
+                htmlAns.style.backgroundColor = highlightColor;
+                htmlAns.textContent += ' (kliknij w to)'
             }
-            for (_i = 0, wszystkieOdp_1 = wszystkieOdp; _i < wszystkieOdp_1.length; _i++) {
-                ans = wszystkieOdp_1[_i];
-                if (ans.textContent && ans.textContent.toLowerCase().slice(3) == (odpowiedz === null || odpowiedz === void 0 ? void 0 : odpowiedz.odpowiedz.toLowerCase()) && ans.className != 'odpgood') {
-                    htmlAns = ans;
-                    if (clickButtons) {
-                        htmlAns.click();
-                    }
-                    else {
-                        htmlAns.style.backgroundColor = highlightColor;
-                        htmlAns.textContent += ' (kliknij w to)';
-                    }
-                }
-            }
-            setTimeout(nowePytanie, delay, odpowiedzi, { clickButtons: clickButtons, highlightColor: highlightColor, delay: delay });
-            return [2 /*return*/];
-        });
-    });
-};
+        }
+    }
+    setTimeout(nowePytanie, delay, odpowiedzi, {clickButtons, highlightColor, delay});
+}
